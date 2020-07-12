83573688d09b7aa4054d73c415c620060da10339
Piotr Musiał <3049399-PiotrMusial@users.noreply.gitlab.com>
2020-07-12 18:23:57 +0200

    Add Budget and Item types

--- module1/budget_1.go
+++ module1/budget_1.go
@@ -1,5 +1,14 @@
 package module1
 
+type Item struct {
+	Description string
+	Price float32
+}
+
 // Budget stores budget information
+type Budget struct {
+	Max float32
+	Items []Item
+}
 
 // Item stores item information
